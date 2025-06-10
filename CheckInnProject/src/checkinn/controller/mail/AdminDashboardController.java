fc8364286a16767b56cde7e92baa9f808e423b7e
Nischal Gautam <gautamnischal2061@gmail.com>
2025-06-11 05:42:13 +0545

    initializing admindashboard controller

--- CheckInnProject/src/checkinn/controller/mail/AdminDashboardController.java
+++ CheckInnProject/src/checkinn/controller/mail/AdminDashboardController.java
@@ -1,5 +1,12 @@
-package checkinn.controller.mail;
+package checkinn.controller;
+
+import checkinn.view.mail.AdminDashboard;
+import java.awt.event.ActionEvent;
 
 public class AdminDashboardController {
+    private final AdminDashboard view;
 
-}
+    public AdminDashboardController(AdminDashboard view) {
+        this.view = view;
+        initializeListeners();
+    }
