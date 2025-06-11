04b688c29d73f27c870890982390f50c4701a3f8
Nischal Gautam <gautamnischal2061@gmail.com>
2025-06-11 05:47:00 +0545

    initialized logout option for admin

--- CheckInnProject/src/checkinn/controller/mail/AdminDashboardController.java
+++ CheckInnProject/src/checkinn/controller/mail/AdminDashboardController.java
@@ -10,3 +10,28 @@ public class AdminDashboardController {
         this.view = view;
         initializeListeners();
     }
+}
+ private void initializeListeners() {
+    
+        this.view.addLogoutListener((ActionEvent e) -> {
+            logout();
+        });
+    }
+   private void logout() {
+        int confirm = JOptionPane.showConfirmDialog(view, "Are you sure you want to logout?", "Confirm Logout", JOptionPane.YES_NO_OPTION);
+        if (confirm == JOptionPane.YES_OPTION) {
+            // Close the admin dashboard
+            view.dispose();
+            
+            // Re-open the login screen
+            LoginView loginView = new LoginView();
+            UserDao userDao = new UserDao();
+            new LoginController(loginView, userDao).open();
+        }
+    }
+
+    public void showView() {
+        this.view.setLocationRelativeTo(null);
+        this.view.setVisible(true);
+    }
+}
