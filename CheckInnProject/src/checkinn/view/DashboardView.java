c6c0c9d5c341854d3cdd8aa676e7ea45a19ac498
Manita <manitaniraula046@gmail.com>
2025-06-11 08:18:00 +0545

    Admin Dashboard UI

--- CheckInnProject/src/checkinn/view/DashboardView.java
+++ CheckInnProject/src/checkinn/view/DashboardView.java
@@ -56,7 +56,6 @@ public class DashboardView extends javax.swing.JFrame {
         UserNameLabel = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new java.awt.Dimension(1347, 821));
 
         jPanel2.setBackground(new java.awt.Color(23, 194, 157));
 
