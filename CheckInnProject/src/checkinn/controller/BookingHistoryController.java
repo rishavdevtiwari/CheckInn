e9ca000c4270b4fb592521b1a1e4d00f633244ea
rishavdevtiwari <rishavtiwari.ai@gmail.com>
2025-06-15 20:14:36 +0545

    Creation of Booking History Controller File

---
+++ CheckInnProject/src/checkinn/controller/BookingHistoryController.java
@@ -0,0 +1,25 @@
+public class BookingHistoryController {
+    private final BookingHistoryView view;
+    private final UserData user;
+    private final DashboardController dashboardController;
+
+    public BookingHistoryController(BookingHistoryView view, UserData user, DashboardController dashboardController) {
+        this.view = view;
+        this.user = user;
+        this.dashboardController = dashboardController;
+        initializeListeners();
+    }
+
+    public void open() {
+        // Optionally: loadBookingHistory();
+        view.setLocationRelativeTo(null);
+        view.setVisible(true);
+    }
+
+    private void initializeListeners() {
+        view.addBackToDashboardListener(e -> {
+            view.dispose();
+            dashboardController.open();
+        });
+    }
+}
