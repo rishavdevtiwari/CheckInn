c394aac8d5942d9999a0c1c0db64e46f77166db9
Saurav mainali <sauravmainali83@gmail.com>
2025-06-18 12:40:40 +0545

    Functions to get and set values created in model

--- CheckInnProject/src/checkinn/model/Review.java
+++ CheckInnProject/src/checkinn/model/Review.java
@@ -4,10 +4,23 @@
  */
 package checkinn.model;
 
+import java.util.Date;
 /**
  *
  * @author saura
  */
 public class Review {
+    private int reviewId;
+    private int userId;
+    private String reviewText;
+    private Date reviewDate;
     
+    public int getReviewId() { return reviewId; }
+    public void setReviewId(int reviewId) { this.reviewId = reviewId; }
+    public int getUserId() { return userId; }
+    public void setUserId(int userId) { this.userId = userId; }
+    public String getReviewText() { return reviewText; }
+    public void setReviewText(String reviewText) { this.reviewText = reviewText; }
+    public Date getReviewDate() { return reviewDate; }
+    public void setReviewDate(Date reviewDate) { this.reviewDate = reviewDate; }
 }
