d6fbc0852fcdb3ea52d1b0a76d651e63a1a393b2
Saurav mainali <sauravmainali83@gmail.com>
2025-06-18 09:50:44 +0545

    UI TouchUp

--- CheckInnProject/src/checkinn/view/ReviewView.java
+++ CheckInnProject/src/checkinn/view/ReviewView.java
@@ -29,8 +29,8 @@ public class ReviewView extends javax.swing.JFrame {
         jSeparator1 = new javax.swing.JSeparator();
         jLabel1 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
-        jButton1 = new javax.swing.JButton();
+        ReviewsTextArea = new javax.swing.JTextArea();
+        ReviewSubmitButton = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
 
@@ -39,17 +39,19 @@ public class ReviewView extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Times New Roman", 1, 48)); // NOI18N
         jLabel1.setText("REVIEWS");
 
-        jTextArea1.setColumns(20);
-        jTextArea1.setLineWrap(true);
-        jTextArea1.setRows(5);
-        jTextArea1.setWrapStyleWord(true);
-        jScrollPane1.setViewportView(jTextArea1);
+        ReviewsTextArea.setColumns(20);
+        ReviewsTextArea.setLineWrap(true);
+        ReviewsTextArea.setRows(5);
+        ReviewsTextArea.setWrapStyleWord(true);
+        jScrollPane1.setViewportView(ReviewsTextArea);
 
-        jButton1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jButton1.setText("SUBMIT");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        ReviewSubmitButton.setBackground(new java.awt.Color(0, 153, 153));
+        ReviewSubmitButton.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        ReviewSubmitButton.setForeground(new java.awt.Color(255, 255, 255));
+        ReviewSubmitButton.setText("SUBMIT");
+        ReviewSubmitButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                ReviewSubmitButtonActionPerformed(evt);
             }
         });
 
@@ -84,7 +86,7 @@ public class ReviewView extends javax.swing.JFrame {
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(ReviewSubmitButton, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(275, 275, 275))
         );
         layout.setVerticalGroup(
@@ -101,16 +103,16 @@ public class ReviewView extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 319, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(38, 38, 38)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(ReviewSubmitButton, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(50, 50, 50))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void ReviewSubmitButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ReviewSubmitButtonActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_ReviewSubmitButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -148,12 +150,12 @@ public class ReviewView extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton ReviewSubmitButton;
+    private javax.swing.JTextArea ReviewsTextArea;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JSeparator jSeparator1;
-    private javax.swing.JTextArea jTextArea1;
     // End of variables declaration//GEN-END:variables
 }
