c1929e50d8b94871cc16674abde9ec0e08f1ae68
Saurav mainali <sauravmainali83@gmail.com>
2025-06-18 13:23:08 +0545

    UI title and location initialization

--- CheckInnProject/src/checkinn/view/ReviewView.java
+++ CheckInnProject/src/checkinn/view/ReviewView.java
@@ -14,6 +14,8 @@ public class ReviewView extends javax.swing.JFrame {
      * Creates new form ReviewView
      */
     public ReviewView() {
+                setTitle("Leave a Review | CheckInn");
+                        setLocationRelativeTo(null);
         initComponents();
     }
 
