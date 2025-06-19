4ebdf12d6462ebab077e696cf40cf143db62257e
Sukrant <sukrantkharel1@gmail.com>
2025-06-19 21:33:09 +0545

    testing reviews being updated to the database after a user adds a review

--- CheckInnProject/test/ReviewTest.java
+++ CheckInnProject/test/ReviewTest.java
@@ -14,5 +14,22 @@ public class ReviewTest {
         reviewDao = new ReviewDao();
     }
 
-   
+    @Test
+    public void testAddAndGetAllReviews() {
+        // Preparing review
+        Review review = new Review();
+        review.setUserId(1); // Using a valid user ID from the database (Only testing after registering a user)
+        review.setReviewText("JUnit test review " + System.currentTimeMillis());
+
+        // Adding review
+        reviewDao.addReview(review);
+
+        // Fetching reviews and checking for reviews updation
+        List<Review> reviews = reviewDao.getAllReviews();
+        assertFalse(reviews.isEmpty());
+        Review lastReview = reviews.get(0); //fetching reviews
+
+        assertEquals(review.getUserId(), lastReview.getUserId());
+        assertEquals(review.getReviewText(), lastReview.getReviewText());
+    }
 }
