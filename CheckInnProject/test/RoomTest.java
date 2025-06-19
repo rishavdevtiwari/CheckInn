e9b6450e9fe2f86a168de63057f1ebcc0e0d6aa1
Sukrant <sukrantkharel1@gmail.com>
2025-06-19 21:26:13 +0545

    setting up room test java class

--- CheckInnProject/test/RoomTest.java
+++ CheckInnProject/test/RoomTest.java
@@ -1,12 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author ACEWR
- */
+import checkinn.dao.RoomDao;
+import org.junit.*;
+import static org.junit.Assert.*;
+
 public class RoomTest {
+
+    private RoomDao roomDao;
+
+    @Before
+    public void setUp() {
+        roomDao = new RoomDao();
+    }
+
     
-}
