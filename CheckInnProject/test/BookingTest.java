d3bc2bb7db203e708bc9f152ad8ed9febf612081
Saurav mainali <sauravmainali83@gmail.com>
2025-06-20 13:51:54 +0545

    First test for booking

--- CheckInnProject/test/BookingTest.java
+++ CheckInnProject/test/BookingTest.java
@@ -16,4 +16,27 @@ public class BookingTest {
         bookingDao = new BookingDao();
     }
 
+
+
+    @Test
+    public void testSaveAndGetAllBookings() {
+        // initializing booking
+        Booking booking = new Booking();
+        booking.setRoomId(1); 
+        booking.setUserId(1);
+        booking.setStatusId(2); 
+        booking.setInvoiceId(0);
+        booking.setCheckInDate(new Date(System.currentTimeMillis() + 3600 * 1000)); // 1 hour from now
+        booking.setCheckOutDate(new Date(System.currentTimeMillis() + 2 * 3600 * 1000)); // 2 hours from now
+        booking.setTotalPrice(1234.56);
+
+        testBookingId = bookingDao.saveBooking(booking);
+        assertTrue(testBookingId > 0);
+
+        List<Booking> allBookings = bookingDao.getAllBookings();
+        assertNotNull(allBookings);
+        assertTrue(allBookings.stream().anyMatch(b -> b.getBookingId() == testBookingId));
+    }
+
+    
 }
